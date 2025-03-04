3ef5a2e828ff6bad2cb7b9510e6a92e05cafcb1e
Bart Massey <bart.massey@gmail.com>
2025-03-04 13:52:50 -0800

    almost got threads working

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
+use std::thread;
+
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
 const SIZE: usize = 1024*1024*1024;
+const THREADS: usize = 2;
 
 fn main() {
-    let mut seed_rng = StdRng::from_os_rng();
-    let mut seed: Vec<u8> = Vec::with_capacity(SIZE);
-    seed.resize(SIZE, 0);
-    for v in &mut seed {
-        *v = seed_rng.random();
-    }
-    println!("{}", seed.iter().fold(0, |a, v| v.wrapping_add(a)));
+    let mut rands: Vec<u8> = Vec::with_capacity(SIZE);
+    rands.resize(SIZE, 0);
+
+    thread::scope(|scope| {
+        let (first, second) = rands.split_at_mut(SIZE / 2);
+        for nt in 0..THREADS {
+            scope.spawn(|| {
+                let section = if nt == 0 {
+                    first
+                } else {
+                    second
+                };
+                let mut seed_rng = StdRng::from_os_rng();
+                for v in section {
+                    *v = seed_rng.random();
+                }
+            });
+        }
+    });
+
+    println!("{}", rands.iter().fold(0, |a, v| v.wrapping_add(a)));
 }
