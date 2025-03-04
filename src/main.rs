06d7037888a05299badd09f8f5321f511ae1e01c
Bart Massey <bart.massey@gmail.com>
2025-03-04 14:47:28 -0800

    added argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::thread;
 
+use clap::Parser;
+extern crate parse_size;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
-const SIZE: usize = 1024*1024*1024;
-const THREADS: usize = 2;
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(short, long, default_value="1GB", value_parser=|s: &str| parse_size::parse_size(s))]
+    size: u64,
+    #[arg(short, long, default_value="1")]
+    threads: usize,
+}
 
 fn main() {
-    let mut rands = vec![0u8; SIZE];
+    let args = Args::parse();
+    let size = args.size as usize;
+    let mut rands = vec![0u8; size];
 
     thread::scope(|scope| {
-        for rands in rands.chunks_mut(SIZE / THREADS) {
+        for rands in rands.chunks_mut(size / args.threads) {
             scope.spawn(|| {
                 let mut seed_rng = StdRng::from_os_rng();
                 for v in rands {
